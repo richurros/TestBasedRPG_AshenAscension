ffab7d55594bee354a4dfd58a792d9af8796301e
richurros <richardrkim11@gmail.com>
2022-05-17 18:59:28 -0700

    Organized Player Class and deleted extra files

--- Player.h
+++ Player.h
@@ -1,14 +1,39 @@
+#pragma once
+
 #include "Character.h"
+#include "item.h"
+#include <vector>
+#include "armor.h"
+#include "weapon.h"
+#include "room.h"
+
+using namespace std;
 
 class Player : public Character
 {
 	private:
-		string type;
+   	 vector<Item> vInventory;
+   	 Weapon weaponEq;
+   	 Armor armorEq;
+	 string Type;
+    	 Room location;
+    	 int combatPt;
 
 	public:
-		Player(string playerClass, string name, int maxHealth, int atk, int defense, int spd);
-		void setType(string playerClass);
+	  Player(string playerClass, string name, int maxHealth, int atk, int defense, int spd, Weapon starterw, Armor startera, Room starter);
+	  void setType(string playerClass);
 		string getType() { return type; }
+    	  void setPlayerClass(string c);
+	  void printInv();
+	  void addToInv(Item n);
+	  void rmFromInv(Item n);
+	  Item getItem(unsigned);
+    	  int getcombatPt() {return combatPt; }
+    	  void eqWeapon(Weapon w);
+    	  void eqArmor(Armor a);
+    	  void move();
+    	  int attack(Enemy x);
+    
 
 };
 
