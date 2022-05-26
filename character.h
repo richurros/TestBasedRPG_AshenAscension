a18ba0acdf43bfb03802496a1718cae86f4bd910
mmart505 <103087631+mmart505@users.noreply.github.com>
2022-05-26 11:02:59 -0700

    Added lower case versions for consistency

---
+++ character.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <iostream>
+#include <string> 
+using namespace std;
+
+class Character
+{
+private:
+	string name;
+	int maxHealth;
+	int atk;
+	int defense;
+	int spd;
+	int dmgTaken;
+
+public:
+	Character();
+	Character(string name, int maxHealth, int atk, int defense, int spd);
+	void setName(string name);
+	string getName() { return name; }
+	void setMaxHealth(int maxHealth);
+	int getMaxHealth() { return maxHealth; }
+	void setAtk(int atk);
+	int getAtk() { return atk; }
+	void setDefense(int defense);
+	int getDefense() { return defense; }
+	void setSpd(int spd);
+	int getSpd() { return spd; }
+};
+
