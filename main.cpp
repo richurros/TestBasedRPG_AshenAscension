fa72fa924dd529fe352e83173807c8a4b233f6f0
rbhus002 <rbhus002@ucr.edu>
2022-05-26 10:01:56 -0700

    Modified inventory system

--- main.cpp
+++ main.cpp
@@ -1,37 +1,37 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include "player.h"
 #include "item.h"
-#include "character.h"
-#include "sample.h"
 #include "bag.h"
 
 
 int main() {
-	Sample i1;
-	Bag b1;
+	Item* i1 = new Item("Item", "Desc", 1);
+	Bag* b1 = new Bag("Bag Name", "Bag Desc", 1);
 
-	b1.setName("Bag A");
-	b1.setDesc("For Testing Purposes");
-	b1.setItemCount(1);
+	//b1.setName("Bag A");
+	//b1.setDesc("For Testing Purposes");
+	//b1.setItemCount(1);
 
-	i1.setName("Sample Name");
-	i1.setDesc("testing");
-	i1.setItemCount(5);
+	//i1.setName("Sample Name");
+	//i1.setDesc("testing");
+	//i1.setItemCount(5);
 
-	Sample i2("Potions", "Heals Character HP", 2);
+	//Sample i2("Potions", "Heals Character HP", 2);
 	
-	Player p1("e", "e", 1, 1, 1, 1);
-	b1.addToBag(i1);
-	b1.addToBag(i2);
+	b1->addToBag(i1);
 	
-	p1.addToInv(b1);
+	Bag* b2 = new Bag("Bag 2", "Bag 2 Desc", 2);
 	
-	//i1.print();
-	b1.printBag();
+	b1->addToBag(b2);	
+
+	b1->print();
 	cout << endl << endl;
-	p1.printInv();
-	
+	b1->printBag();
+        cout << endl << endl;
+	//b1->vBag.at(1)->printBag();
+	//cout << endl << endl;
+	delete b1;
+
 	return 0;
 }
--- weapon.h
+++ weapon.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <string>
-#include <item.h>
+#include "item.h"
 
 using namespace std;
 
